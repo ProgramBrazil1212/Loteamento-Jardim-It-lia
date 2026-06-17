578d94bb6e61591fef3dd5ace56fda14bd035b37
ProgramBrazil1212 <bruscatolucas@gmail.com>
2026-06-16 22:05:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Banco de dados completo baseado no documento oficial do Jardim Itália
+// Banco de dados oficial completo - Loteamento Jardim Itália
 const bancoLoteamento = {
     "Q15": [
         { lote: "1", area: "505,96", dono: "FAMÍLIA", tel: "" },
@@ -10,70 +10,263 @@ const bancoLoteamento = {
         { lote: "7", area: "363,90", dono: "LOTEADORES", tel: "" },
         { lote: "8", area: "362,14", dono: "FAMÍLIA", tel: "" },
         { lote: "9", area: "402,56", dono: "FAMÍLIA", tel: "" },
-        { lote: "10", area: "785,75", dono: "FAMÍLIA", tel: "" }
-    ], // 
+        { lote: "10", area: "785,75", dono: "FAMÍLIA", tel: "" },
+        { lote: "11", area: "362,61", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "454,28", dono: "LOTEADORES", tel: "" },
+        { lote: "13", area: "362,83", dono: "FAMÍLIA", tel: "" },
+        { lote: "14", area: "361,12", dono: "LOTEADORES", tel: "" },
+        { lote: "15", area: "362,34", dono: "FAMÍLIA", tel: "" },
+        { lote: "16", area: "379,11", dono: "LOTEADORES", tel: "" },
+        { lote: "17", area: "418,92", dono: "FAMÍLIA", tel: "" },
+        { lote: "18", area: "440,14", dono: "LOTEADORES", tel: "" },
+        { lote: "19", area: "445,17", dono: "LOTEADORES", tel: "" },
+        { lote: "20", area: "450,21", dono: "FAMÍLIA", tel: "" },
+        { lote: "21", area: "455,37", dono: "LOTEADORES", tel: "" }
+    ],
     "Q16": [
         { lote: "1", area: "450,90", dono: "FAMÍLIA", tel: "" },
         { lote: "2", area: "362,25", dono: "LOTEADORES", tel: "" },
         { lote: "3", area: "362,25", dono: "FAMÍLIA", tel: "" },
         { lote: "4", area: "362,25", dono: "LOTEADORES", tel: "" },
-        { lote: "5", area: "362,25", dono: "FAMÍLIA", tel: "" }
-    ], // [cite: 406]
+        { lote: "5", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "9", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "10", area: "360,67", dono: "FAMÍLIA", tel: "" },
+        { lote: "11", area: "455,17", dono: "LOTEADORES", tel: "" },
+        { lote: "12", area: "452,60", dono: "FAMÍLIA", tel: "" },
+        { lote: "13", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "14", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "15", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "16", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "17", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "18", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "19", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "20", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "21", area: "455,17", dono: "LOTEADORES", tel: "" }
+    ],
     "Q17": [
         { lote: "1", area: "455,46", dono: "FAMÍLIA", tel: "" },
         { lote: "2", area: "361,93", dono: "LOTEADORES", tel: "" },
-        { lote: "3", area: "362,25", dono: "FAMÍLIA", tel: "" }
-    ], // [cite: 408]
+        { lote: "3", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "4", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "5", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "7", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "9", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "11", area: "451,50", dono: "LOTEADORES", tel: "" },
+        { lote: "12", area: "451,50", dono: "FAMÍLIA", tel: "" },
+        { lote: "13", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "14", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "15", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "16", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "17", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "18", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "19", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "20", area: "362,25", dono: "LOTEADORES", tel: "" },
+        { lote: "21", area: "362,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "22", area: "513,12", dono: "LOTEADORES", tel: "" }
+    ],
     "Q18": [
         { lote: "1", area: "397,26", dono: "LOTEADORES", tel: "" },
         { lote: "2", area: "399,25", dono: "FAMÍLIA", tel: "" },
-        { lote: "3", area: "483,77", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 409]
+        { lote: "3", area: "483,77", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "478,48", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "453,85", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "7", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "386,83", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "369,21", dono: "LOTEADORES", tel: "" },
+        { lote: "10", area: "360,14", dono: "FAMÍLIA", tel: "" },
+        { lote: "11", area: "483,16", dono: "LOTEADORES", tel: "" },
+        { lote: "12", area: "754,61", dono: "FAMÍLIA", tel: "" },
+        { lote: "13", area: "747,83", dono: "FAMÍLIA", tel: "" }
+    ],
     "Q19": [
         { lote: "1", area: "452,65", dono: "FAMÍLIA", tel: "" },
         { lote: "2", area: "366,06", dono: "LOTEADORES", tel: "" },
-        { lote: "5", area: "457,17", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 411]
+        { lote: "3", area: "363,68", dono: "FAMÍLIA", tel: "" },
+        { lote: "4", area: "394,39", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "457,17", dono: "LOTEADORES", tel: "" },
+        { lote: "6", area: "372,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "372,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "LOTEADORES", dono: "MIGUEL", tel: "" },
+        { lote: "9", area: "375,91", dono: "LOTEADORES", tel: "" },
+        { lote: "10", area: "375,83", dono: "LOTEADORES", tel: "" }
+    ],
     "Q20": [
         { lote: "1", area: "451,46", dono: "FAMÍLIA", tel: "" },
         { lote: "2", area: "360,00", dono: "LOTEADORES", tel: "" },
-        { lote: "3", area: "360,00", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 412]
+        { lote: "3", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "8", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "10", area: "360,80", dono: "FAMÍLIA", tel: "" },
+        { lote: "11", area: "465,53", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "381,33", dono: "FAMÍLIA", tel: "" },
+        { lote: "13", area: "464,71", dono: "LOTEADORES", tel: "" },
+        { lote: "14", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "15", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "16", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "17", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "18", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "19", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "20", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "21", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "22", area: "484,54", dono: "LOTEADORES", tel: "" }
+    ],
     "Q21": [
         { lote: "1", area: "450,37", dono: "FAMÍLIA", tel: "" },
         { lote: "2", area: "360,55", dono: "LOTEADORES", tel: "" },
         { lote: "3", area: "451,28", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 413]
+    ],
     "Q22": [
         { lote: "1", area: "462,00", dono: "LOTEADORES", tel: "" },
         { lote: "2", area: "360,00", dono: "FAMÍLIA", tel: "" },
-        { lote: "3", area: "360,00", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 414]
+        { lote: "3", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "6", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "361,21", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "361,28", dono: "LOTEADORES", tel: "" },
+        { lote: "9", area: "366,40", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "366,41", dono: "LOTEADORES", tel: "" },
+        { lote: "11", area: "458,26", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "369,38", dono: "LOTEADORES", tel: "" },
+        { lote: "13", area: "367,06", dono: "FAMÍLIA", tel: "" },
+        { lote: "14", area: "361,83", dono: "FAMÍLIA", tel: "" },
+        { lote: "15", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "16", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "17", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "18", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "19", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "20", area: "462,00", dono: "FAMÍLIA", tel: "" }
+    ],
     "Q23": [
         { lote: "1", area: "450,36", dono: "LOTEADORES", tel: "" },
-        { lote: "2", area: "360,00", dono: "FAMÍLIA", tel: "" }
-    ], // [cite: 416]
+        { lote: "2", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "3", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "5", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "7", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "9", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "11", area: "459,75", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "375,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "13", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "14", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "15", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "16", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "17", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "18", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "19", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "20", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "21", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "22", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "23", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "24", area: "450,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "25", area: "450,00", dono: "LOTEADORES", tel: "" },
+        { lote: "26", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "27", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "28", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "29", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "30", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "31", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "32", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "33", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "34", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "35", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "36", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "37", area: "645,36", dono: "LOTEADORES", tel: "" },
+        { lote: "38", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "39", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "40", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "41", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "42", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "43", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "44", area: "360,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "45", area: "360,00", dono: "LOTEADORES", tel: "" },
+        { lote: "46", area: "468,60", dono: "FAMÍLIA", tel: "" }
+    ],
     "Q24": [
         { lote: "1", area: "462,59", dono: "FAMÍLIA", tel: "" },
-        { lote: "2", area: "364,00", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 418]
+        { lote: "2", area: "364,00", dono: "LOTEADORES", tel: "" },
+        { lote: "3", area: "364,00", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "364,00", dono: "LOTEADORES", tel: "" },
+        { lote: "5", area: "364,00", dono: "LOTEADORES", tel: "" },
+        { lote: "6", area: "384,61", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "384,61", dono: "LOTEADORES", tel: "" },
+        { lote: "8", area: "384,61", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "438,60", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "386,03", dono: "LOTEADORES", tel: "" },
+        { lote: "11", area: "515,24", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "363,97", dono: "LOTEADORES", tel: "" },
+        { lote: "13", area: "363,97", dono: "FAMÍLIA", tel: "" },
+        { lote: "14", area: "362,83", dono: "LOTEADORES", tel: "" },
+        { lote: "15", area: "360,55", dono: "FAMÍLIA", tel: "" },
+        { lote: "16", area: "465,84", dono: "LOTEADORES", tel: "" }
+    ],
     "Q25": [
         { lote: "1", area: "1536,04", dono: "LOTEADORES", tel: "" },
-        { lote: "4", area: "366,26", dono: "FAMÍLIA", tel: "" }
-    ], // [cite: 419]
+        { lote: "2", area: "366,26", dono: "LOTEADORES", tel: "" },
+        { lote: "3", area: "366,26", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "366,26", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "373,82", dono: "FAMÍLIA", tel: "" }
+    ],
     "Q26": [
         { lote: "1", area: "360,01", dono: "FAMÍLIA", tel: "" },
-        { lote: "3", area: "360,39", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 421]
+        { lote: "2", area: "360,25", dono: "FAMÍLIA", tel: "" },
+        { lote: "3", area: "360,39", dono: "LOTEADORES", tel: "" },
+        { lote: "4", area: "362,44", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "360,91", dono: "LOTEADORES", tel: "" },
+        { lote: "6", area: "360,79", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "360,49", dono: "LOTEADORES", tel: "" },
+        { lote: "8", area: "585,95", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "403,39", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "420,00", dono: "LOTEADORES", tel: "" },
+        { lote: "11", area: "420,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "420,00", dono: "LOTEADORES", tel: "" },
+        { lote: "13", area: "420,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "14", area: "420,00", dono: "LOTE EM COMUM", tel: "" },
+        { lote: "15", area: "420,00", dono: "FAMÍLIA", tel: "" },
+        { lote: "16", area: "420,00", dono: "LOTEADORES", tel: "" }
+    ],
     "Q27": [
         { lote: "1", area: "369,75", dono: "FAMÍLIA", tel: "" },
-        { lote: "4", area: "369,75", dono: "LOTEADORES", tel: "" }
-    ], // [cite: 420]
+        { lote: "2", area: "369,75", dono: "FAMÍLIA", tel: "" },
+        { lote: "3", area: "369,75", dono: "FAMÍLIA", tel: "" },
+        { lote: "4", area: "369,75", dono: "LOTEADORES", tel: "" },
+        { lote: "5", area: "369,75", dono: "FAMÍLIA", tel: "" },
+        { lote: "6", area: "369,75", dono: "LOTEADORES", tel: "" },
+        { lote: "7", area: "394,63", dono: "FAMÍLIA", tel: "" },
+        { lote: "8", area: "432,79", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "441,70", dono: "FAMÍLIA", tel: "" },
+        { lote: "10", area: "438,51", dono: "LOTEADORES", tel: "" },
+        { lote: "11", area: "435,32", dono: "FAMÍLIA", tel: "" },
+        { lote: "12", area: "432,14", dono: "LOTEADORES", tel: "" },
+        { lote: "13", area: "673,01", dono: "FAMÍLIA", tel: "" },
+        { lote: "14", area: "419,89", dono: "LOTEADORES", tel: "" }
+    ],
     "Q28": [
         { lote: "1", area: "538,55", dono: "FAMÍLIA", tel: "" },
+        { lote: "2", area: "465,31", dono: "FAMÍLIA", tel: "" },
         { lote: "3", area: "445,99", dono: "EDU", tel: "" },
-        { lote: "7", area: "366,17", dono: "RENATO", tel: "" }
-    ] // [cite: 422]
+        { lote: "4", area: "439,77", dono: "FAMÍLIA", tel: "" },
+        { lote: "5", area: "395,90", dono: "FERGUTZ", tel: "" },
+        { lote: "6", area: "360,51", dono: "FAMÍLIA", tel: "" },
+        { lote: "7", area: "366,17", dono: "RENATO", tel: "" },
+        { lote: "8", area: "367,95", dono: "FAMÍLIA", tel: "" },
+        { lote: "9", area: "373,34", dono: "EDU", tel: "" },
+        { lote: "10", area: "375,66", dono: "FAMÍLIA", tel: "" }
+    ]
 };
 
 function abrirPainelQuadra(quadraId) {
@@ -81,15 +274,16 @@ function abrirPainelQuadra(quadraId) {
     const corpoTabela = document.getElementById("lista-lotes-corpo");
     
     document.getElementById("modal-titulo-quadra").innerText = "📋 Lotes da " + quadraId.replace("Q", "Quadra ");
-    corpoTabela.innerHTML = ""; // Limpa a tabela anterior
+    corpoTabela.innerHTML = ""; 
 
     if (lotes) {
         lotes.forEach(l => {
+            let donoClasse = l.dono.toLowerCase().replace(/í/g, 'i').replace(/ /g, '-');
             let linha = `<tr>
                 <td><strong>Lote ${l.lote}</strong></td>
                 <td>${l.area} m²</td>
-                <td><span class="badge-${l.dono.toLowerCase().replace('í','i')}">${l.dono}</span></td>
-                <td><input type="text" class="input-tel" value="${l.tel}" placeholder="Adicionar telefone..." onchange="atualizarTelefone('${quadraId}', '${l.lote}', this.value)"></td>
+                <td><span class="badge-${donoClasse}">${l.dono}</span></td>
+                <td><input type="text" class="input-tel" value="${l.tel}" placeholder="Inserir contato..." onchange="atualizarTelefone('${quadraId}', '${l.lote}', this.value)"></td>
             </tr>`;
             corpoTabela.innerHTML += linha;
         });
@@ -102,8 +296,7 @@ function atualizarTelefone(quadra, lote, novoValor) {
     const item = lista.find(l => l.lote === lote);
     if(item) {
         item.tel = novoValor;
-        console.log(`Telefone do Lote ${lote} da ${quadra} atualizado para: ${novoValor}`);
-        // Dica: Aqui no futuro você pode salvar isso numa API ou banco real.
+        console.log(`Contato Lote ${lote} [${quadra}] alterado para: ${novoValor}`);
     }
 }
 
